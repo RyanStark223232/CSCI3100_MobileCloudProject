0f8dfa0bcd172cab2712aee1ee3880d7c93dfc57
RyanStark223232 <62586619+RyanStark223232@users.noreply.github.com>
2021-03-01 20:56:49 +0800

    Added Template SignIn
    Added Template SignIn

--- nowhere/src/App.js
+++ nowhere/src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './SignIn.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about">Home</Link>
+            </li>
+            <li>
+              <Link to="/try">Click Here</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <SignIn />
+          </Route>
+          <Route path="/try">
+            <Unfinished />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Unfinished() {
+  return (
+    <div>
+      <h1>Underconstruction</h1>
+      <img src={logo} alt="logo"></img>
     </div>
   );
 }
