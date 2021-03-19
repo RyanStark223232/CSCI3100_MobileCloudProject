ba830e0fd02a7232c9d3187b5293b01e3f2fb6fc
RyanStark223232 <62586619+RyanStark223232@users.noreply.github.com>
2021-03-19 23:30:02 +0800

    Updated CreatePost.js

--- nowhere/src/CreatePost.js
+++ nowhere/src/CreatePost.js
@@ -35,27 +35,31 @@ class CreatePost extends React.Component {
                 </h1>
                 <div className="wrapper">
                   <div className="left-item"><span>Title:</span></div>
-                  <div className="right-item"><input size="60" type="text" placeholder="Enter a title"></input></div>
+                  <div className="right-item"><input size="60" type="text" placeholder="Enter something..."></input></div>
                 </div>
                 <div className="wrapper">
                   <div className="left-item"><span>Description:</span></div>
-                  <div className="right-item"><textarea rows="5"></textarea></div>
+                  <div className="right-item"><textarea rows="5" placeholder="Enter something..."></textarea></div>
                 </div>
                 <div className="wrapper">
                   <div className="left-item"><span>Location:</span></div>
-                  <div className="right-item"><input type="text"></input></div>
+                  <div className="right-item"><input type="text" placeholder="Enter something..."></input></div>
                 </div>
                 <div className="wrapper">
-                  <div className="left-item"><span>Quota:</span></div>
-                  <div className="right-item"><input type="text"></input></div>
+                  <div className="left-item"><span>Size:</span></div>
+                  <div className="right-item"><input type="text" placeholder="Enter something..."></input></div>
                 </div>
                 <div className="wrapper">
                   <div className="left-item"><span>Period:</span></div>
-                  <div className="right-item"><input type="date"></input> to <input type="date"></input></div>
+                  <div className="right-item"><input type="text" placeholder="Enter something..."></input></div>
+                </div>
+                <div className="wrapper">
+                  <div className="left-item"><span>Travelling Style:</span></div>
+                  <div className="right-item"><input type="text" placeholder="Enter something..."></input></div>
                 </div>
                 <div className="wrapper">
                   <div className="left-item"><span>Remark:</span></div>
-                  <div className="right-item"><textarea rows="3"></textarea></div>
+                  <div className="right-item"><textarea rows="3" placeholder="Enter something..."></textarea></div>
                 </div>
                 <div className="wrapper">
                   <div className="left-item">Upload Images:</div>
