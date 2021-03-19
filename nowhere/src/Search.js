04381a2dd447651f133d97ae644a58c3160988ca
RyanStark223232 <62586619+RyanStark223232@users.noreply.github.com>
2021-03-19 12:01:33 +0800

    Merge branch 'main' of https://github.com/RyanStark223232/CSCI3100_MobileCloudProject into main

--- nowhere/src/Search.js
+++ nowhere/src/Search.js
@@ -18,11 +18,12 @@ class Search extends React.Component {
         this.onChange = this.onChange.bind(this);
         this.loadImage = this.loadImage.bind(this);
         this.load = this.load.bind(this);
-        this.save = this.save.bind(this);
         const knnClassifier = require('@tensorflow-models/knn-classifier');
         this.classifier = knnClassifier.create();
     }
 
+    // Code for training, Commented out since it is useless now
+    /*
     loadTrain = async(event) =>{
         require('@tensorflow/tfjs-backend-webgl');
         var url = URL.createObjectURL(event.target.files[0]);
@@ -35,8 +36,11 @@ class Search extends React.Component {
         await this.classifier.addExample(activation, label);
         console.log("Example Added");
     }
+    */
 
+    // Upon uploading an image, it perform neural network inference and find closest location
     loadImage = async(event) =>{
+        // Obtain TFJS backend
         require('@tensorflow/tfjs-backend-webgl');
         if ((event.target.files[0]) == null){return};
         var url = URL.createObjectURL(event.target.files[0]);
@@ -44,22 +48,24 @@ class Search extends React.Component {
         const img = document.getElementById('image');
         const mobilenet = require('@tensorflow-models/mobilenet');
 
+        // Download MobilenetV2
         const model = await mobilenet.load();
         const predictions = await model.classify(img);      
 
+        // Get Default Prediction Result
         console.log('Predictions: ');
         console.log(predictions);
-
         this.setState({p1:predictions[0].className,
                        p2:predictions[1].className,
                        p3:predictions[2].className
         })
 
+        // Classify Most Similar Places
         if (this.classifier.getNumClasses() > 0) {      
             // Get the activation from mobilenet from the webcam.
             const inf_activation = model.infer(img, 'conv_preds');
             // Get the most likely class and confidence from the classifier module.
-            const result = await this.classifier.predictClass(inf_activation);
+            const result = await this.classifier.predictClass(inf_activation, 1);
             this.setState({place:result.label});
             console.log(result);
         }
@@ -70,6 +76,8 @@ class Search extends React.Component {
         console.log(event);
     }
 
+    // Code for saving training, Commented out since it is useless now
+    /*
     save() {
         let dataset = this.classifier.getClassifierDataset()
         var datasetObj = {}
@@ -80,21 +88,25 @@ class Search extends React.Component {
           datasetObj[key] = Array.from(data); 
         });
         let jsonStr = JSON.stringify(datasetObj)
-        //can be change to other source
-        localStorage.setItem("myData", jsonStr);
+        //localStorage.setItem("myData", jsonStr);
+        const blob = new Blob([jsonStr], {type: "application/json"});
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.download = "myData.json";
+        link.href = url;
+        link.click();
         console.log("Model Saved");
     }
+    */
 
     load() {
-        //can be change to other source
-       let dataset = localStorage.getItem("myData")
-       let tensorObj = JSON.parse(dataset)
-       //covert back to tensor
-       Object.keys(tensorObj).forEach((key) => {
-         tensorObj[key] = tf.tensor(tensorObj[key], [tensorObj[key].length / 1024, 1024])
-       })
-       this.classifier.setClassifierDataset(tensorObj);
-       console.log("Model Loaded");
+        // Load Transfer Learning Trained Model
+        require('@tensorflow/tfjs-backend-webgl');
+        var data = require('./myData.json')
+        console.log(data)
+        let tensorObj = data
+        this.classifier.setClassifierDataset(tensorObj);
+        console.log("Model Loaded");
     }
 
     render(){
@@ -110,24 +122,16 @@ class Search extends React.Component {
                     <input type="file" id="input_image" onChange={this.loadImage}/>
                 </div>
                 <div>
-                    <input type="file" id="train_image" onChange={this.loadTrain}/>
-                    <input type="text" id="train_class" name="train_class"></input>
-                </div>
-                <div>
-                    <button onClick={this.save} id="button1">Save</button>
                     <button onClick={this.load} id="button2">Load</button>
                 </div>
                 <div>
                     <img src={this.state.image} id="image" crossOrigin="anonymous" alt="test"></img>
                 </div>
                 <div>
-                    <img src={this.state.train} id="train" crossOrigin="anonymous" alt="train"></img>
-                </div>
-                <div>
-                    <h5>1st: {this.state.p1}</h5>
-                    <h5>2nd: {this.state.p2}</h5>
-                    <h5>3rd: {this.state.p3}</h5>
-                    <h1>Self Trained: {this.state.place}</h1>
+                    <h5>Similar 1st: {this.state.p1}</h5>
+                    <h5>Similar 2nd: {this.state.p2}</h5>
+                    <h5>Similar 3rd: {this.state.p3}</h5>
+                    <h1>Similar Location: {this.state.place}</h1>
                 </div>
             </header>
         );
