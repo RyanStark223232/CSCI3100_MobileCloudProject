48adab8f9a753a8d094d72194b32c90c51ba76d7
RyanStark223232 <62586619+RyanStark223232@users.noreply.github.com>
2021-03-19 23:17:14 +0800

    Removed Test Data

--- header.js
+++
@@ -1,5 +0,0 @@
-var http = require("http");
-http.createServer(function(request, response) {
-console.log(request.headers);
-response.end();
-}).listen(8000);
--- http.js
+++
@@ -1,4 +0,0 @@
-var http = require("http");
-var server = http.createServer(function(request, response){response.write("Hello!");
-							   response.end();});
-server.listen(8000);
