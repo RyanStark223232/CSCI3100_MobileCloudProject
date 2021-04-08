574a9c82cc061514510f2eacc05632cc34d73ecd
potatoLam <fe7ng654123@gmail.com>
2021-04-08 13:01:35 +0800

    hide sign in tab after logging in

--- nowhere/src/App.js
+++ nowhere/src/App.js
@@ -1,3 +1,15 @@
+/* 
+CSCI3100 Project NowHere
+Group D2
+
+LIN Chuanfeng 1155110077
+name sid....
+name sid....
+name sid....
+name sid....
+*/
+
+
 import './App.css';
 import SignIn from './SignIn.js';
 import SignUp from './SignUp.js';
@@ -8,15 +20,39 @@ import CreatePost from "./CreatePost.js";
 import EditProfile from "./EditProfile.js";
 import Button from '@material-ui/core/Button';
 import nowhere from './Logo_Temp.jpg';
-// import Firebase from "./Firebase";
+import React from 'react';
+import auth from "./Firebase";
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import { render } from '@testing-library/react';
+
+
+
+export function setLogedIn() {
+  console.log("in setloged in");
+  document.getElementById("beforeAuth").style.display = "none";
+  document.getElementById("afterAuth").style.display = "inline";
+}
+
+export function setLogedOut() {
+  console.log("in setloged out");
+  document.getElementById("beforeAuth").style.display = "inline";
+  document.getElementById("afterAuth").style.display = "none";
+  auth.signOut();
+}
 
 function App() {
+  // class App extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+
+  //   }
+  //   render(){
   return (
     <header>
       <Router>
@@ -25,27 +61,36 @@ function App() {
             <img src={nowhere} alt="logo" class="logo"></img>
           </Link>
           <div class="accountSetting">
-            <Link to="/signup" > 
-              <Button variant="contained" size="small" color="primary">
-                Sign Up
+            <div id="afterAuth" class="toHide">
+              <Link to="/accountdetails" >
+                <Button variant="contained" size="small" color="primary">
+                  Accountdetails
+            </Button>
+              </Link>
+              <Link to="/editprofile" >
+                <Button variant="contained" size="small" color="primary">
+                  Edit Profile
+            </Button>
+              </Link>
+              <Link to="/">
+                <Button variant="contained" size="small" color="primary" onClick={setLogedOut}>
+                  Log Out
+            </Button>
+              </Link>
+            </div>
+            <div id="beforeAuth" >
+              <Link to="/signup" >
+                <Button variant="contained" size="small" color="primary" >
+                  Sign Up
               </Button>
-            </Link>
-            <Link to="/signin" > 
-              <Button variant="contained" size="small" color="primary">
-                Sign In
+              </Link>
+              <Link to="/signin" >
+                <Button variant="contained" size="small" color="primary" onClick={setLogedIn}>
+                  Sign In
               </Button>
-            </Link>
-            <Link to="/accountdetails" > 
-              <Button variant="contained" size="small" color="primary">
-                Accountdetails
-              </Button>
-            </Link>
-            <Link to="/editprofile" >
-              <Button variant="contained" size="small" color="primary">
-                Edit Profile
-              </Button>
-            </Link>
-          </div>          
+              </Link>
+            </div>
+          </div>
         </div>
         <div>
           <nav>
@@ -65,7 +110,7 @@ function App() {
                   <Button variant="outlined" size="large">Create Post</Button>
                 </Link>
               </li>
-              
+
             </ul>
           </nav>
 
@@ -88,7 +133,7 @@ function App() {
 
             <Route path="/editprofile" component={EditProfile}>
             </Route>
-            
+
             <Route path="/" component={Home}>
             </Route>
 
@@ -100,7 +145,9 @@ function App() {
     </header>
 
   );
+
 }
+// }
 
 export default App;
-// withRouter(observer(MyComponent))
+
--- nowhere/src/SignIn.js
+++ nowhere/src/SignIn.js
@@ -3,7 +3,7 @@
 */
 
 
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -21,24 +21,12 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
 import auth from "./Firebase";
-import {Comment, Functions} from "@material-ui/icons";
+import { Comment, Functions } from "@material-ui/icons";
 import { IconButton } from '@material-ui/core';
+import App, { setLogedIn } from './App';
 
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="textSecondary" align="center">
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://material-ui.com/">
-//         Your Website
-//       </Link>{' '}
-//       {new Date().getFullYear()}
-//       {'.'}
-//     </Typography>
-//   );
-// }
 
-// const useStyles = makeStyles((theme) => ({
-  const styles = theme => ({
+const useStyles = theme => ({
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
@@ -59,245 +47,126 @@ import { IconButton } from '@material-ui/core';
 });
 
 class SignIn extends React.Component {
-  constructor(props){
-      super(props);
-      // this.onChange = this.onChange.bind(this);
-      this.emailRef = React.createRef();
-      this.pwRef = React.createRef();
-      // this.classes = useStyles();
+  constructor(props) {
+    super(props);
+    // this.onChange = this.onChange.bind(this);
+    this.emailRef = React.createRef();
+    this.pwRef = React.createRef();
   }
 
 
 
 
-    handleSignin  = async(e) =>{
-      e.preventDefault()
-    try{
-       await auth.signInWithEmailAndPassword(this.emailRef.current.value, this.pwRef.current.value).then(value => {
-        if (auth.currentUser != null){
-          alert("sign in succeeded with email ="+ this.emailRef.current.value )
-        }else {
+  handleSignin = async (e) => {
+    e.preventDefault()
+    try {
+      await auth.signInWithEmailAndPassword(this.emailRef.current.value, this.pwRef.current.value).then(value => {
+        if (auth.currentUser != null) {
+          alert("sign in succeeded with email =" + this.emailRef.current.value)
+        } else {
           alert("login failed")
         }
-      }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            if (errorCode === 'auth/wrong-password') {
-              alert('Wrong password.');
-            } else {
-              alert(errorMessage);
-            }
-            console.log(error);
-          })
-    }catch (e) {
+      }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode === 'auth/wrong-password') {
+          alert('Wrong password.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+      })
+    } catch (e) {
       alert(e)
     }
-    
-    
+
+
   };
 
-   logOut() {
-      console.log("in LogOut()")
-    auth.signOut()
-  };
+  //  logOut() {
+  //     console.log("in LogOut()")
+  //   auth.signOut()
+  //   setLogedIn();
+  // };
 
-  render(){
+  render() {
     // const { classes } = this.props;
     const classes = this.props.classes;
-    console.log(classes);
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
+    // console.log(classes);
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
         </Typography>
-        <form onSubmit={this.handleSignin} className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            inputRef= {this.emailRef}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            inputRef= {this.pwRef}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
+          <form onSubmit={this.handleSignin} className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              inputRef={this.emailRef}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              inputRef={this.pwRef}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
             <Grid container>
-              <h1 onClick={this.logOut}> 
-              <Link href="#" variant="body2">
-                {"\nlog out"}
-              </Link></h1>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+              </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        {/*<Copyright />*/}
-      </Box>
-    </Container>
-  );
+          </form>
+        </div>
+        <Box mt={8}>
+        </Box>
+      </Container>
+    );
   }
 }
 
-// export default function SignIn() {
-//   const classes = useStyles();
-//   const emailRef = useRef()
-//   const pwRef = useRef()
 
-//   async function handleSignin(e) {
-//       e.preventDefault()
-//     try{
-//        await auth.signInWithEmailAndPassword(emailRef.current.value, pwRef.current.value).then(value => {
-//         if (auth.currentUser != null){
-//           alert("sign in succeeded with email ="+ emailRef.current.value )
-//         }else {
-//           alert("login failed")
-//         }
-//       }).catch(function(error) {
-//             // Handle Errors here.
-//             var errorCode = error.code;
-//             var errorMessage = error.message;
-//             if (errorCode === 'auth/wrong-password') {
-//               alert('Wrong password.');
-//             } else {
-//               alert(errorMessage);
-//             }
-//             console.log(error);
-//           })
-//     }catch (e) {
-//       alert(e)
-//     }
-    
-    
-//   }
-
-//   function logOut() {
-//       console.log("in LogOut()")
-//     auth.signOut()
-//   }
-
-//   return (
-//     <Container component="main" maxWidth="xs">
-//       <CssBaseline />
-//       <div className={classes.paper}>
-//         <Avatar className={classes.avatar}>
-//           <LockOutlinedIcon />
-//         </Avatar>
-//         <Typography component="h1" variant="h5">
-//           Sign in
-//         </Typography>
-//         <form onSubmit={handleSignin} className={classes.form} noValidate>
-//           <TextField
-//             variant="outlined"
-//             margin="normal"
-//             required
-//             fullWidth
-//             id="email"
-//             label="Email Address"
-//             name="email"
-//             autoComplete="email"
-//             autoFocus
-//             inputRef= {emailRef}
-//           />
-//           <TextField
-//             variant="outlined"
-//             margin="normal"
-//             required
-//             fullWidth
-//             name="password"
-//             label="Password"
-//             type="password"
-//             id="password"
-//             autoComplete="current-password"
-//             inputRef= {pwRef}
-//           />
-//           <FormControlLabel
-//             control={<Checkbox value="remember" color="primary" />}
-//             label="Remember me"
-//           />
-//           <Button
-//             type="submit"
-//             fullWidth
-//             variant="contained"
-//             color="primary"
-//             className={classes.submit}
-//           >
-//             Sign In
-//           </Button>
-//           <Grid container>
-//             <Grid item xs>
-//               <Link href="#" variant="body2">
-//                 Forgot password?
-//               </Link>
-//             </Grid>
-//             <Grid item>
-//               <Link href="/signup" variant="body2">
-//                 {"Don't have an account? Sign Up"}
-//               </Link>
-//             </Grid>
-//             <Grid container>
-//               <h1 onClick={logOut}> 
-//               <Link href="#" variant="body2">
-//                 {"\nlog out"}
-//               </Link></h1>
-//             </Grid>
-//           </Grid>
-//         </form>
-//       </div>
-//       <Box mt={8}>
-//         {/*<Copyright />*/}
-//       </Box>
-//     </Container>
-//   );
-// }
-
-// export class SignIn;
 
 SignIn.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SignIn);
+export default withStyles(useStyles)(SignIn);
